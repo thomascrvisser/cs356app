4a89a14424be1c6b6fcdd1060f43fd9d3532c02c
Sam Smith <smittysport7@gmail.com>
2020-11-06 16:42:59 -0700

    added background color

--- HomeScreen.js
+++ HomeScreen.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { StyleSheet, Text, View, SafeAreaView, TouchableOpacity, Image, Dimensions } from 'react-native';
+import { LinearGradient } from 'expo-linear-gradient'
 
 const DATA = {
   title: 'Test User'
@@ -11,41 +12,49 @@ const onSettingPress = () => {};
 export default function HomeScreen() {
   return (
     <View style={styles.container}>
-      <SafeAreaView>
-        <View style={styles.homePageHeaderSection}>
-          <Text style={styles.headerUserName}>{DATA.title}</Text>
-          <TouchableOpacity style={styles.button} activeOpacity={0.5} onPress={onSettingPress}>
-            <Image
-              style={styles.buttonImageIconStyle}
-              source={require('./assets/settings_icon.png')}
-            />
-          </TouchableOpacity>
-        </View>
-        {/* Add the list here, replace it with the TEXT field */}
-        <Text style={styles.generalText}>Hello and Welcome! Add the score card list here</Text>
-      </SafeAreaView>
+      <LinearGradient
+        colors={['steelblue', 'lightgray']}
+        style={styles.container}
+        start={{ x: 0, y: 0 }}
+        end={{ x: 0, y: 1 }}
+      >
+        <SafeAreaView>
+          <View style={styles.homePageHeaderSection}>
+            <Text style={styles.headerUserName}>{DATA.title}</Text>
+            <TouchableOpacity style={styles.button} activeOpacity={0.5} onPress={onSettingPress}>
+              <Image
+                style={styles.buttonImageIconStyle}
+                source={require('./assets/settings_icon.png')}
+              />
+            </TouchableOpacity>
+          </View>
+          {/* Add the list here, replace it with the TEXT field */}
+          <Text style={styles.generalText}>Hello and Welcome! Add the score card list here</Text>
+        </SafeAreaView>
+      </LinearGradient>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
-    backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
   },
   generalText: {
     flex: 2,
-    color: "blue",
+    color: "white",
     fontSize: 50,
     textAlign: "center"
   },
   homePageHeaderSection: {
     width: window.width,
     height: 70,
-    backgroundColor: 'skyblue',
+    backgroundColor: 'whitesmoke',
     alignItems: 'center',
-    flexDirection: 'row'
+    flexDirection: 'row',
+    borderWidth: 2,
+    borderRadius: 10
   },
   headerUserName: {
     fontSize: 30,
@@ -53,7 +62,6 @@ const styles = StyleSheet.create({
     marginLeft: 30
   },
   button: {
-    backgroundColor: "#DDDDDD",
     padding: 10,
     marginHorizontal: 160
   },

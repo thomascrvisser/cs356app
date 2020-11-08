5e279fce5d10dee202ba5cd3b51f3d7c0a110331
Sam Smith <smittysport7@gmail.com>
2020-11-07 21:15:12 -0700

    major updates to navigation structure and project structure

--- screens/Home.js
+++ screens/Home.js
@@ -1,28 +1,27 @@
 import React from 'react';
 import { StyleSheet, Text, View, SafeAreaView, TouchableOpacity, Image, Dimensions } from 'react-native';
 import { LinearGradient } from 'expo-linear-gradient'
+import { Ionicons } from '@expo/vector-icons';
 
 const window = Dimensions.get('window');
 const onSettingPress = () => {};
 
 
-export default function HomeScreen({ navigation }) {
+export default function Home({ route, navigation }) {
+  const { username } = route.params
   return (
     <View style={styles.container}>
       <LinearGradient
         colors={['steelblue', 'lightgray']}
         style={styles.container}
         start={{ x: 0, y: 0 }}
-        end={{ x: 0, y: 1 }}
+        end={{ x: 0, y: 1 }} 
       >
         <SafeAreaView>
           <View style={styles.homePageHeaderSection}>
-            <Text style={styles.headerUserName}>{navigation.getParam('username')}</Text>
+            <Text style={styles.headerUserName}>{username}</Text>
             <TouchableOpacity style={styles.button} activeOpacity={0.5} onPress={onSettingPress}>
-              <Image
-                style={styles.buttonImageIconStyle}
-                source={require('./assets/settings_icon.png')}
-              />
+              <Ionicons name='ios-cog' color={'black'} size={40}/>
             </TouchableOpacity>
           </View>
           {/* Add the list here, replace it with the TEXT field */}

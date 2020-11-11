0ca1e76ff7b91109374647169b4c2e70a04288ee
thomascrvisser <thomascrvisser@gmail.com>
2020-11-10 18:52:13 -0700

    create screen

--- screens/Create.js
+++ screens/Create.js
@@ -1,19 +1,184 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from 'react';
+import { StyleSheet, Text, View, FlatList } from 'react-native';
+import { ScrollView, TextInput, TouchableHighlight, TouchableOpacity } from 'react-native-gesture-handler';
+import { Counter } from 'react-native-counters'
+import { CheckBox } from 'react-native-elements';
+import { sub } from 'react-native-reanimated';
 
-export default function Create() {
-  return (
-    <View style={styles.container}>
-      <Text>Create</Text>
+export default class Create extends React.Component {
+  state = {
+      name: "",
+      desc: "",
+      players: 1,
+      columns: 0,
+      columnHeaders: {},
+      disable: true
+    }
+  
+
+  render(){
+
+    const convertText = (text) => {
+      var mynum = parseInt(text);
+      if (this.state.columnHeaders.length != this.state.columns) {
+        this.setState({columnHeaders: {}});
+      }
+      this.setState({columns: mynum});
+    }
+
+    const submit = () => {
+      console.log(this.state);
+    }
+
+    const textCheck = () => {
+      if (text == ""){
+        this.setState({disable: true})
+      }else{
+        this.setState({disable: false})
+      }
+    }
+
+    
+
+    const updatecolstate = (text, j) => {
+      var oldCol = this.state.columnHeaders
+      oldCol[j] = text
+      this.setState({columnHeaders: oldCol});
+      if (Object.keys(this.state.columnHeaders).length == this.state.columns){
+        this.setState({disable: false})
+      }
+      else{
+        this.setState({disable: true})
+      }
+
+      //if you delete text from cell disable button
+      if (text == ""){
+        this.setState({disable: true})
+      }
+    }
+
+    const getCols = () => {
+      let i = 0;
+      let wtf = []
+
+      while (i < this.state.columns){
+        let j = (i+1).toString();
+        wtf.push(<TextInput 
+          key = {j}
+          style={styles.colContainer} 
+          placeholder={j}
+          onChangeText={text => updatecolstate(text, j)}
+         ></TextInput>);
+        i++;
+      }
+      console.log(this.state);
+      return(wtf);
+    }
+
+    return (
+      <View style={styles.container}>
+        <View style={styles.pad}>
+          <Text style={styles.title}>Name of Game</Text>
+          <TextInput 
+            style={styles.inputcontainer} 
+            placeholder="Enter the game name..."
+            onChangeText={text => this.setState({name:text})}></TextInput>
+        </View>
+        <View style={styles.pad}>
+          <Text style={styles.title}>Description</Text>
+          <TextInput 
+            style={styles.inputcontainer} 
+            placeholder="Enter the rules of the game..."
+            onChangeText={text => this.setState({desc:text})}></TextInput>
+        </View>
+        <View style={styles.pad}>
+          <Text style={styles.title} ># of Players</Text>
+          <TextInput 
+            style={styles.numbercontainer}
+            keyboardType="numeric"
+            placeholder="1"
+            onChangeText={text => this.setState({players: parseInt(text)})}></TextInput>
+        </View>
+        <View style={styles.pad}>
+          <Text style={styles.title} ># of Columns (the number of rounds)</Text>
+          <TextInput 
+            style={styles.numbercontainer}
+            keyboardType="numeric"
+            placeholder="1"
+            onChangeText={text => convertText(text)}></TextInput>
+        </View>
+        <Text>Enter column names</Text>
+        <ScrollView horizontal={true}>
+          <View style={{flexDirection:'row'}}>
+            {getCols()}
+        </View></ScrollView>
+
+        <TouchableOpacity disabled={this.state.disable} onPress={submit}>
+          <Text style={[ this.state.disable ? styles.loginBtnD : styles.loginBtn]}>Submit</Text>
+        </TouchableOpacity>
+        
+        
     </View>
-  );
+    );
+  }
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    paddingLeft: 30
+  },
+  pad: {
+    paddingBottom: 15
+  },
+  title: {
+   paddingBottom: 5
+  },
+  numbercontainer: {
+    borderLeftWidth: 2,
+    borderRightWidth: 2,
+    borderTopWidth: 2,
+    borderBottomWidth: 2,
+    width:50,
+    height: 40,
+    textAlign: 'center'
+  },
+  inputcontainer: {
+    borderLeftWidth: 2,
+    borderRightWidth: 2,
+    borderTopWidth: 2,
+    borderBottomWidth: 2,
+    width:300,
+    height: 40,
+  },
+  colContainer: {
+    borderLeftWidth: 2,
+    borderRightWidth: 1,
+    borderTopWidth: 2,
+    borderBottomWidth: 2,
+    width:100,
+    height: 40,
+    textAlign: 'center'
+  },
+  loginBtn:{
+    width:"80%",
+    backgroundColor:"#fb5b5a",
+    borderRadius:25,
+    height:50,
+    alignItems:"center",
+    justifyContent:"center",
+    marginTop:40,
+    marginBottom:10
+  },
+  loginBtnD:{
+    width:"80%",
+    backgroundColor:"#808080",
+    borderRadius:25,
+    height:50,
+    alignItems:"center",
+    justifyContent:"center",
+    marginTop:40,
+    marginBottom:10
   },
 });

8c2450007bc1851088d0fb2def73de52f79092f8
thomascrvisser <thomascrvisser@gmail.com>
2020-11-24 18:59:42 -0700

    adding changes

--- screens/Create.js
+++ screens/Create.js
@@ -1,39 +1,64 @@
 import React, { useState } from 'react';
-import { StyleSheet, Text, View, FlatList } from 'react-native';
+import { StyleSheet, Text, View, FlatList, Keyboard, TouchableWithoutFeedback } from 'react-native';
 import { ScrollView, TextInput, TouchableHighlight, TouchableOpacity } from 'react-native-gesture-handler';
+import InputSpinner from 'react-native-input-spinner';
 import { testUser1 } from '../db';
 import { scoreCardService } from '../models/scorecard';
+
 
 export default class Create extends React.Component {
   state = {
       name: "",
       desc: "",
       players: 1,
-      columns: 0,
+      columns: 1,
       columnHeaders: {},
-      disable: true
+      disable: false
     }
   
 
   render(){
-    const { navigate } = this.props.navigation
-    const convertText = (text) => {
-      var mynum = parseInt(text);
-      if (this.state.columnHeaders.length != this.state.columns) {
-        this.setState({columnHeaders: {}});
+    const initState = {
+      name: "",
+      desc: "",
+      players: 1,
+      columns: 1,
+      columnHeaders: {},
+      disable: false
+    }
+
+    const createHeaderArray = () => {
+      let headarray = []
+      // if user didn't customize column names, create default
+      if (Object.keys(this.state.columnHeaders).length == 0){
+        for(var i = 0; i < this.state.columns; i++){
+          headarray.push(`${i+1}`)
+        }
       }
-      this.setState({columns: mynum});
+      //otherwise build the header array
+      else{ 
+        for (var key in this.state.columnHeaders){
+          headarray.push(this.state.columnHeaders[key])
+        }
+      }
+      return headarray;
+    }
+
+    const createName = () => {
+      if(this.state.name == ""){
+        var num = Math.floor((Math.random() * 10000 + 1))
+        return `Game ${num}`
+      }
+      else{
+        return this.state.name
+      }
     }
 
     const submit = () => {
-      //build header array
-      let headarray = []
-      for (var key in this.state.columnHeaders){
-        headarray.push(this.state.columnHeaders[key])
-      }
-
+      let headarray = createHeaderArray();
+      let name = createName();
       let newScorecard = new scoreCardService(
-        this.state.name,
+        name,
         this.state.desc,
         this.state.players,
         headarray.length,
@@ -41,37 +66,14 @@ export default class Create extends React.Component {
       )
 
       testUser1.push(newScorecard)
-
-      navigate('Home', {
-        screen: 'Home',
-      })
-    }
-
-    const textCheck = () => {
-      if (text == ""){
-        this.setState({disable: true})
-      }else{
-        this.setState({disable: false})
-      }
-    }
-
-    
+      console.log(this.state.name);
+      this.props.navigation.replace('Home');
+    }    
 
     const updatecolstate = (text, j) => {
       var oldCol = this.state.columnHeaders
       oldCol[j] = text
       this.setState({columnHeaders: oldCol});
-      if (Object.keys(this.state.columnHeaders).length == this.state.columns){
-        this.setState({disable: false})
-      }
-      else{
-        this.setState({disable: true})
-      }
-
-      //if you delete text from cell disable button
-      if (text == ""){
-        this.setState({disable: true})
-      }
     }
 
     const getCols = () => {
@@ -88,44 +90,50 @@ export default class Create extends React.Component {
          ></TextInput>);
         i++;
       }
-      //console.log(this.state);
       return(wtf);
     }
 
     return (
-      <View style={styles.container}>
-        <View style={styles.pad}>
-          <Text style={styles.title}>Name of Game</Text>
-          <TextInput 
-            style={styles.inputcontainer} 
-            placeholder="Enter the game name..."
-            onChangeText={text => this.setState({name:text})}></TextInput>
-        </View>
-        <View style={styles.pad}>
-          <Text style={styles.title}>Description</Text>
-          <TextInput 
-            style={styles.inputcontainer} 
-            placeholder="Enter the rules of the game..."
-            onChangeText={text => this.setState({desc:text})}></TextInput>
-        </View>
-        <View style={styles.pad}>
-          <Text style={styles.title} ># of Columns (the number of rounds)</Text>
-          <TextInput 
-            style={styles.numbercontainer}
-            keyboardType="numeric"
-            placeholder="1"
-            onChangeText={text => convertText(text)}></TextInput>
-        </View>
-        <Text>Enter column names</Text>
-        <ScrollView horizontal={true}>
-          <View style={{flexDirection:'row'}}>
-            {getCols()}
-        </View></ScrollView>
+        <View style={styles.container}>
+            <View style={styles.pad}>
+              <Text style={styles.title}>Name of Game</Text>
+              <TextInput 
+                style={styles.inputcontainer} 
+                placeholder="Enter the game name..."
+                onChangeText={text => this.setState({name:text})}></TextInput>
+            </View>
+            <View style={styles.pad}>
+              <Text style={styles.title}>Description</Text>
+              <TextInput 
+                style={styles.inputcontainer} 
+                placeholder="Enter rules or general info about the game..."
+                onChangeText={text => this.setState({desc:text})}></TextInput>
+            </View>
+            <View style={styles.pad}>
+              <Text style={styles.title} ># of Rounds</Text>
+              <InputSpinner
+                max={20}
+                min={1}
+                step={1}
+                color={"#fb5b5a"}
+                value={this.state.columns}
+                onChange={(num) => {
+                  this.setState({columns:num});
+                }}
+              />
+            </View>
+            <Text>Customize Round Names</Text>
+            <ScrollView horizontal={true}>
+              <View style={{flexDirection:'row', flexWrap: 'wrap', alignItems:"flex-start"}}>
+                {getCols()}
+              </View>
+            </ScrollView>
 
-        <TouchableOpacity disabled={this.state.disable} onPress={submit}>
-          <Text style={[ this.state.disable ? styles.loginBtnD : styles.loginBtn]}>Submit</Text>
-        </TouchableOpacity>       
-    </View>
+            <TouchableOpacity disabled={this.state.disable} onPress={submit}>
+              <Text style={styles.loginBtn}>Submit</Text>
+            </TouchableOpacity>       
+        </View>
+      
     );
   }
 }
@@ -134,7 +142,9 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    paddingLeft: 30
+    alignItems: "center",
+    width: "100%",
+    alignSelf:'baseline'
   },
   pad: {
     paddingBottom: 15
@@ -147,7 +157,6 @@ const styles = StyleSheet.create({
     borderRightWidth: 2,
     borderTopWidth: 2,
     borderBottomWidth: 2,
-    width:50,
     height: 40,
     textAlign: 'center'
   },
@@ -156,14 +165,17 @@ const styles = StyleSheet.create({
     borderRightWidth: 2,
     borderTopWidth: 2,
     borderBottomWidth: 2,
+
     width:300,
     height: 40,
   },
   colContainer: {
     borderLeftWidth: 2,
-    borderRightWidth: 1,
+    borderRightWidth: 2,
     borderTopWidth: 2,
     borderBottomWidth: 2,
+    marginLeft: 5,
+    marginRight: 5,
     width:100,
     height: 40,
     textAlign: 'center'
@@ -175,17 +187,8 @@ const styles = StyleSheet.create({
     height:50,
     alignItems:"center",
     justifyContent:"center",
+    width:300,
     marginTop:40,
     marginBottom:10
-  },
-  loginBtnD:{
-    width:"80%",
-    backgroundColor:"#808080",
-    borderRadius:25,
-    height:50,
-    alignItems:"center",
-    justifyContent:"center",
-    marginTop:40,
-    marginBottom:10
-  },
+  }
 });
